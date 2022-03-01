0ec67fa5dc37da53021df2e56bd7e423cc394052
jungtin-fpt <thinhntse140344@fpt.edu.vn>
2022-03-01 16:37:32 +0700

    [WA-2] Update DataInit

--- src/main/java/com/dfksoft/hrm_manage/data/DataInitAppRunner.java
+++ src/main/java/com/dfksoft/hrm_manage/data/DataInitAppRunner.java
@@ -36,57 +36,66 @@ public class DataInitAppRunner implements ApplicationRunner {
         /*
          *   ======== Role ==========
          * */
-        roleService.createRole("ROLE_ADMIN", "Administrator of dashboard");
-        roleService.createRole("ROLE_MANAGER", "Manager of dashboard");
-        roleService.createRole("ROLE_USER", "A normal account with basic permission");
+        if (roleService.getAllRole().size() == 0) {
+            roleService.createRole("ROLE_ADMIN", "Administrator of dashboard");
+            roleService.createRole("ROLE_MANAGER", "Manager of dashboard");
+            roleService.createRole("ROLE_USER", "A normal account with basic permission");
+        }
         
         /*
          *   ======== Company ==========
          * */
-        Company company = new Company();
-        company.setName("KMS Company");
-        company.setDescription(
-            "KMS Technology is a leading provider of offshore software development, testing services, and consulting");
-        company.setStatus("Available");
-        companyRepository.save(company);
-        
-        Company companyTwo = new Company();
-        companyTwo.setName("NET Company");
-        company.setDescription(
-            "Netcompany was founded in 2000 and has its headquarters in Copenhagen, Denmark. Today, we are an international company with more than 3000 employees in 6 countries");
-        companyTwo.setStatus("Available");
-        companyRepository.save(companyTwo);
+        if (companyRepository.findAll().size() == 0) {
+            Company company = new Company();
+            company.setName("KMS Company");
+            company.setDescription(
+                "KMS Technology is a leading provider of offshore software development, testing services, and consulting");
+            company.setStatus("Available");
+            companyRepository.save(company);
+            
+            Company companyTwo = new Company();
+            companyTwo.setName("NET Company");
+            company.setDescription(
+                "Netcompany was founded in 2000 and has its headquarters in Copenhagen, Denmark. Today, we are an international company with more than 3000 employees in 6 countries");
+            companyTwo.setStatus("Available");
+            companyRepository.save(companyTwo);
+        }
         
         /*
          *   ======== Title ==========
          * */
-        titleService.createTitle("Software Engineer",
-            "Design and create computer systems and applications to solve real-world problems",
-            LocalTime.now());
-        titleService.createTitle("Marketing Analyst",
-            "An individual who works to identify goods & services desired by a set of customers as well as marketing of those goods and services on behalf of the company",
-            LocalTime.now());
-    
+        if (titleService.getAllTitle().size() == 0) {
+            titleService.createTitle("Software Engineer",
+                "Design and create computer systems and applications to solve real-world problems",
+                LocalTime.now());
+            titleService.createTitle("Marketing Analyst",
+                "An individual who works to identify goods & services desired by a set of customers as well as marketing of those goods and services on behalf of the company",
+                LocalTime.now());
+        }
+        
         /*
          *   ======== Location ==========
          * */
-        locationService.initLocation("KMS Tản Viên", "123 Cong Hoa Tan Binh Ho Chi Minh", 1, 1);
-        locationService.initLocation("KMS Trường Chinh", "1 Đ. Trường Chinh, Tây Thạnh, Tân Phú, Thành phố Hồ Chí Minh", 1, 1);
-    
+        if (locationService.getAllLocation().size() == 0) {
+            locationService.initLocation("KMS Tản Viên", "123 Cong Hoa Tan Binh Ho Chi Minh", 1, 1);
+            locationService.initLocation("KMS Trường Chinh",
+                "1 Đ. Trường Chinh, Tây Thạnh, Tân Phú, Thành phố Hồ Chí Minh", 1, 1);
+        }
+        
         /*
          *   ======== Account ==========
          * */
-        accountService.initAccount(
-            "admin",
-            BCrypt.hashpw("admin", BCrypt.gensalt()),
-            1, 1, 1, 1
-        );
-        accountService.initAccount(
-            "user",
-            BCrypt.hashpw("user", BCrypt.gensalt()),
-            3, 2, 1, 1
-        );
-        
-        
+        if (accountService.getAllAccount().size() == 0) {
+            accountService.initAccount(
+                "admin",
+                BCrypt.hashpw("admin", BCrypt.gensalt()),
+                1, 1, 1, 1
+            );
+            accountService.initAccount(
+                "user",
+                BCrypt.hashpw("user", BCrypt.gensalt()),
+                3, 2, 1, 1
+            );
+        }
     }
 }
